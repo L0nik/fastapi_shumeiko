9df7c9d8bc1cc226372cdcac28e64ae93f3bb1e2
l0nik <l0nik@mail.ru>
2025-07-03 19:32:32 +0300

    Подключение базы данных

--- src/main.py
+++ src/main.py
@@ -8,8 +8,7 @@ sys.path.append(str(Path(__file__).parent.parent))
 
 from src.api.hotels import router as router_hotels
 from src.config import settings
-
-print(f"{settings.DB_NAME}")
+from src.database import *
 
 app = FastAPI(docs_url=None)
 app.include_router(router_hotels)
