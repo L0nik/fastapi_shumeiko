a9dda59d9e7661e0e273024bdf09e7539f97835d
l0nik <l0nik@mail.ru>
2025-07-04 18:41:44 +0300

    модель отеля, миграции

--- src/database.py
+++ src/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
-from sqlalchemy import text
+from sqlalchemy.orm import DeclarativeBase
 import asyncio
 
 from src.config import settings
@@ -9,3 +9,6 @@ engine = create_async_engine(settings.DB_URL)
 async_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False)
 
 session = async_session_maker()
+
+class BaseModel(DeclarativeBase):
+    pass
