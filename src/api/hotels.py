2d52b0e1185ba4732e56ea626f864e3db95b893f
l0nik <l0nik@mail.ru>
2025-07-08 13:53:39 +0300

    запрос в БД на добавление отеля через sqlalchemy

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -1,4 +1,9 @@
 from fastapi import Query, APIRouter, Body
+
+from sqlalchemy import insert
+
+from src.database import async_session_maker, engine
+from src.models.hotels import HotelsModel
 from src.schemas.hotels import Hotel, HotelPATCH
 from src.api.dependencies import PaginationDep
 
@@ -49,14 +54,14 @@ def delete_hotel(hotel_id: int):
     "",
     summary="Создание отеля"
 )
-def create_hotel(
+async def create_hotel(
         hotel_data: Hotel = Body(openapi_examples={
             "1" : {
                 "summary": "Сочи",
                 "description": "",
                 "value": {
                     "title": "Отель Сочи 5 звезд у моря",
-                    "name": "sochi_u_morya"
+                    "location": "ул. Моря, 1"
                 }
             },
             "2" : {
@@ -64,19 +69,17 @@ def create_hotel(
                 "description": "",
                 "value": {
                     "title": "Отель Дубай у фонтана",
-                    "name": "dubai_fountain"
+                    "location": "ул. Шейха, 2"
                 }
             }
         })
 ):
-    global hotels
-    hotels.append(
-        {
-            "id": hotels[-1]["id"] + 1,
-            "title": hotel_data.title,
-            "name": hotel_data.name
-        }
-    )
+    async with async_session_maker() as session:
+        add_hotel_stmt = insert(HotelsModel).values(**hotel_data.model_dump())
+        print(add_hotel_stmt.compile(engine, compile_kwargs={"literal_binds": True}))
+        await session.execute(add_hotel_stmt)
+        await session.commit()
+
     return {"status": "OK"}
 
 @router.put(
