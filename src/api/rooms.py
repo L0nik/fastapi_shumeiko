53e8f63bf2aa7e626b0b38aef529418fe7ae7c0a
l0nik <l0nik@mail.ru>
2025-07-30 15:30:34 +0300

    Решение: функционал номеров

--- src/api/rooms.py
+++ src/api/rooms.py
@@ -2,26 +2,31 @@ from fastapi import APIRouter, Body
 
 from src.database import async_session_maker
 from src.repo.rooms import RoomsRepository
-from src.schemas.rooms import RoomAdd, RoomPatch
+from src.schemas.rooms import RoomAddRequest, RoomPatchRequest, RoomAdd, RoomPatch
 
 router = APIRouter(prefix="/hotels", tags=["Номера"])
 
 @router.get("/{hotel_id}/rooms", summary="Получение номеров отеля")
 async def get_rooms(hotel_id: int):
     async with async_session_maker() as session:
-        rooms = await RoomsRepository(session).get_all(hotel_id=hotel_id)
+        rooms = await RoomsRepository(session).get_filtered(hotel_id=hotel_id)
     return {"status": "OK", "data": rooms}
+
+@router.get("/{hotel_id}/rooms/{room_id}", summary="Получение данных конкретного номера")
+async def get_room(hotel_id: int, room_id: int):
+    async with async_session_maker() as session:
+        room = await RoomsRepository(session).get_one_or_none(hotel_id=hotel_id, id=room_id)
+    return {"status": "OK", "data": room}
 
 @router.post("/{hotel_id}/rooms", summary="Создание номера отеля")
 async def create_room(
         hotel_id: int,
-        room_data: RoomAdd = Body(
+        room_data: RoomAddRequest = Body(
             openapi_examples={
                 "1": {
                     "summary": "Премиум номер",
                     "description": "",
                     "value": {
-                        "hotel_id": 1,
                         "title": "Премиум номер",
                         "description": "Просторный номер с видом на море",
                         "price": 10000,
@@ -31,8 +36,9 @@ async def create_room(
             }
         )
 ):
+    _room_data = RoomAdd(hotel_id=hotel_id, **room_data.model_dump())
     async with async_session_maker() as session:
-        new_room = await RoomsRepository(session).add(room_data)
+        new_room = await RoomsRepository(session).add(_room_data)
         await session.commit()
     return {"status": "OK", "data": new_room}
 
@@ -47,12 +53,11 @@ async def delete_room(hotel_id: int, room_id: int):
 async def put_room(
         hotel_id: int,
         room_id: int,
-        room_data: RoomAdd = Body(openapi_examples={
+        room_data: RoomAddRequest = Body(openapi_examples={
             "1": {
                 "summary": "Эконом номер",
                 "description": "",
                 "value": {
-                    "hotel_id": 1,
                     "title": "Эконом номер",
                     "description": "Компактный номер с видом на море",
                     "price": 3000,
@@ -61,14 +66,16 @@ async def put_room(
             }
         })
 ):
+    _room_data = RoomAdd(hotel_id=hotel_id, **room_data.model_dump())
     async with async_session_maker() as session:
-        await RoomsRepository(session).edit(room_data, hotel_id=hotel_id, id=room_id)
+        await RoomsRepository(session).edit(_room_data, hotel_id=hotel_id, id=room_id)
         await session.commit()
     return {"status": "OK"}
 
 @router.patch("/{hotel_id}/rooms/{room_id}")
-async def patch_room(hotel_id: int, room_id: int, room_data: RoomPatch):
+async def patch_room(hotel_id: int, room_id: int, room_data: RoomPatchRequest):
+    _room_data = RoomPatch(hotel_id=hotel_id, **room_data.model_dump(exclude_unset=True))
     async with async_session_maker() as session:
-        await RoomsRepository(session).edit(room_data, exclude_unset=True, hotel_id=hotel_id, id=room_id)
+        await RoomsRepository(session).edit(_room_data, exclude_unset=True, hotel_id=hotel_id, id=room_id)
         await session.commit()
     return {"status": "OK"}
--- src/schemas/rooms.py
+++ src/schemas/rooms.py
@@ -1,5 +1,11 @@
 from pydantic import BaseModel, ConfigDict, Field
 
+class RoomAddRequest(BaseModel):
+    title: str
+    description: str | None
+    price: int
+    quantity: int
+
 class RoomAdd(BaseModel):
     hotel_id: int
     title: str
@@ -7,12 +13,18 @@ class RoomAdd(BaseModel):
     price: int
     quantity: int
 
+class RoomPatchRequest(BaseModel):
+    title: str | None = None
+    description: str | None = None
+    price: int | None = None
+    quantity: int | None = None
+
 class RoomPatch(BaseModel):
-    hotel_id: int | None = Field(None)
-    title: str | None = Field(None)
-    description: str | None = Field(None)
-    price: int | None = Field(None)
-    quantity: int | None = Field(None)
+    hotel_id: int | None = None
+    title: str | None = None
+    description: str | None = None
+    price: int | None = None
+    quantity: int | None = None
 
 class Room(RoomAdd):
     id: int
