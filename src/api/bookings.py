c232647add7417f4f7752b7d8673a8934646c985
l0nik <l0nik@mail.ru>
2025-08-01 14:37:28 +0300

    Решение: ручка на добавление бронирования

--- src/api/bookings.py
+++ src/api/bookings.py
@@ -3,14 +3,17 @@ from datetime import date
 
 from src.schemas.bookings import BookingAddRequest, BookingAdd
 from src.services.auth import AuthService
-from src.api.dependencies import DBDep
+from src.api.dependencies import DBDep, UserIdDep
 
 router = APIRouter(prefix="/bookings", tags=["Бронирования"])
 
 @router.post("", summary="забронировать номер")
-async def post_booking(db: DBDep, booking_data: BookingAddRequest, request: Request):
-    access_token = request.cookies.get("access_token")
-    user_id = AuthService().decode_token(access_token).get("user_id")
+async def post_booking(
+    user_id: UserIdDep,
+    db: DBDep,
+    booking_data: BookingAddRequest,
+    request: Request
+):
     room_data = await db.rooms.get_one_or_none(id=booking_data.room_id)
     if not room_data:
         raise HTTPException(status_code=404, detail="Номер с таким идентификатором не найден")
