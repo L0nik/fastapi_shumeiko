d7eb69f0df270a1d5ead9153631665db40e0ab71
l0nik <l0nik@mail.ru>
2025-08-04 13:06:25 +0300

    задание: ручки для получения бронирований

--- src/api/bookings.py
+++ src/api/bookings.py
@@ -26,3 +26,13 @@ async def post_booking(
     new_booking = await db.bookings.add(_booking_data)
     await db.commit()
     return {"status": "OK", "data": new_booking}
+
+@router.get("", summary="Получить все бронирования")
+async def get_bookings(db: DBDep):
+    bookings = await db.bookings.get_all()
+    return {"status": "OK", "data": bookings}
+
+@router.get("/me", summary="Получить свои бронирования")
+async def get_my_bookings(db: DBDep, user_id: UserIdDep):
+    bookings = await db.bookings.get_filtered(user_id=user_id)
+    return {"status": "OK", "data": bookings}
