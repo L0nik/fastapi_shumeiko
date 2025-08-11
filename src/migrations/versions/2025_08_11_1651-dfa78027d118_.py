84980568866674bcb2a6411c43399e33d004709b
l0nik <l0nik@mail.ru>
2025-08-11 16:57:05 +0300

    добавлены таблицы facilities и rooms_facilities

---
+++ src/migrations/versions/2025_08_11_1651-dfa78027d118_.py
@@ -0,0 +1,50 @@
+"""empty message
+
+Revision ID: dfa78027d118
+Revises: 2c70b1033d4b
+Create Date: 2025-08-11 16:51:27.797382
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "dfa78027d118"
+down_revision: Union[str, Sequence[str], None] = "2c70b1033d4b"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.create_table(
+        "facilities",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("title", sa.String(length=100), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+    )
+    op.create_table(
+        "rooms_facilities",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("room_id", sa.Integer(), nullable=False),
+        sa.Column("facility_id", sa.Integer(), nullable=False),
+        sa.ForeignKeyConstraint(
+            ["facility_id"],
+            ["facilities.id"],
+        ),
+        sa.ForeignKeyConstraint(
+            ["room_id"],
+            ["rooms.id"],
+        ),
+        sa.PrimaryKeyConstraint("id"),
+    )
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.drop_table("rooms_facilities")
+    op.drop_table("facilities")
---
+++ src/models/facilities.py
@@ -0,0 +1,17 @@
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import mapped_column, Mapped
+
+from src.database import BaseModel
+
+class FacilitiesModel(BaseModel):
+    __tablename__ = "facilities"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(100))
+
+class RoomsFacilitiesModel(BaseModel):
+    __tablename__ = "rooms_facilities"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    room_id: Mapped[int] = mapped_column(ForeignKey("rooms.id"))
+    facility_id: Mapped[int] = mapped_column(ForeignKey("facilities.id"))
